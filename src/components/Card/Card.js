3807c5336bcdd1fbd6ff2e92a126db9ec4b4e2b4
takihama <danielcalderonuns@gmail.com>
2022-04-18 17:51:36 -0300

    Fix flip card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,11 +4,11 @@ const Card = ({ text, isFlipped, clickHandler }) => {
   return (
     <div className='card' onClick={clickHandler}>
       <div className={'card-inner' + (isFlipped ? ' flipped' : '')}>
-        <div className='card-front'>
-          <h1>{text}</h1>
-        </div>
         <div className='card-back'>
           <h1>Logo</h1>
+        </div>
+        <div className='card-front'>
+          <h1>{text}</h1>
         </div>
       </div>
     </div>
